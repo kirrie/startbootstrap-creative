72cf6a582fdef684d37bb1c0d85368eb5f11640b
Juheon Lee <kirrie@gmail.com>
2017-11-25 13:10:29 +0900

    아버지 축의금 계좌 추가

--- js/common.js
+++ js/common.js
@@ -7,4 +7,18 @@
 	if(diffDays >= 0) {
 		$('#head-title').html('D-' + diffDays + '일');
 	}
+
+	var getParameterByName = function(name, url) {
+		    if (!url) url = window.location.href;
+		    name = name.replace(/[\[\]]/g, "\\$&");
+		    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		        results = regex.exec(url);
+		    if (!results) return null;
+		    if (!results[2]) return '';
+			return decodeURIComponent(results[2].replace(/\+/g, " "));
+		};
+
+	if(getParameterByName('utm_campaign') == 'lee-parents') {
+		$('#bank-address').show();
+	}
 })(jQuery);
